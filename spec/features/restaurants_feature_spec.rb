5e16f8acf319cb04feaecd3da928cfb119223e1b
alwinsolanky <alwinsolanky@gmail.com>
2016-08-23 17:35:33 +0100

    delete a resturant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'restaurants' do
 
   context 'no restaurants have been added' do
+
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
@@ -12,7 +13,7 @@ feature 'restaurants' do
 
   context 'restaurants have been added' do
     before do
-      Restaurant.create(name: 'KFC')
+      Restaurant.create(name: 'KFC', description: 'Fast Food')
     end
 
     scenario 'display restaurants' do
@@ -41,8 +42,38 @@ feature 'restaurants' do
      visit '/restaurants'
      click_link 'KFC'
      expect(page).to have_content 'KFC'
+
      expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
   end
 
+  context 'editing restaurants' do
+
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+
+    scenario 'let a user edit a restaurant' do
+     visit '/restaurants'
+     click_link 'Edit KFC'
+     fill_in 'Name', with: 'Kentucky Fried Chicken'
+     fill_in 'Description', with: 'Deep fried goodness'
+     click_button 'Update Restaurant'
+     expect(page).to have_content 'Kentucky Fried Chicken'
+     expect(page).to have_content 'Deep fried goodness'
+     expect(current_path).to eq '/restaurants'
+    end
+
+  end
+
+  context 'deleting restaurants' do
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
+
+  
 end
